61868a2f568419d711f5853145c3e545596e86d7
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2017-05-22 17:20:51 -0400

    add first basic representation of example

--- 1.js
+++ 1.js
@@ -1,3 +1,17 @@
 //If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 //Find the sum of all the multiples of 3 or 5 below 1000.
+
+function multi(num){
+  var num_arr = []
+  for (i = 1; i < num; i++)
+    if (i % 3 === 0){
+      num_arr.push(i);
+    } else if (i % 5 === 0) {
+      num_arr.push(i);
+    }
+  var sum = num_arr.reduce(function(x,y) {return x + y;}, 0);
+  console.log(sum);
+}
+
+multi(10);
