6be24ccd4804cb22eac50aa9e5296f90ed27cb3f
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2017-06-05 17:45:18 -0400

    solve 2.py

---
+++ python_solutions/2.py
@@ -0,0 +1,22 @@
+
+
+
+
+def fib():
+    x = 0
+    y = 1
+
+    total = 0
+
+    # for x in range(0, 4000000):
+    while y < 4000000:
+        z = x
+        x = y
+        y = z + x
+        if y % 2 == 0:
+            total = total + y
+
+    print total
+    print "hey"
+
+fib()
