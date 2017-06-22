a26a27563f587367e825a13469cc721856813476
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2017-06-21 23:20:01 -0400

    solve python 3

--- python_solutions/3.py
+++ python_solutions/3.py
@@ -3,22 +3,13 @@
 #What is the largest prime factor of the number 600851475143 ?
 
 def prime_finder(num):
-    num_list = []
+
+    num_list =[]
 
     for possible_factor in range(2,num):
-        if num % possible_factor == 0:
+        while num % possible_factor == 0:
             num_list.append(possible_factor)
-
-    print num_list
-
-    for num in num_list:
-        if num % 13 == 0:
-            num_list.remove(num)
-
-
-
-
-
+            num = num / possible_factor
 
     print num_list
 
