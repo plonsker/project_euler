503cf041fe98085d9bd34ebc0711aaa7b7dbfe8c
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2017-08-17 13:03:44 -0400

    almost solve 7

--- python_solutions/7.py
+++ python_solutions/7.py
@@ -2,6 +2,17 @@
 #
 # What is the 10 001st prime number?
 
+# def is_prime(num):
+#
+#     for i in range(2,num):
+#         if num % i == 0:
+#             return False
+#     return True
+#
+# print is_prime(7)
+
+
+
 def is_prime(num):
 
     for i in range(2,num):
@@ -9,16 +20,42 @@ def is_prime(num):
             return False
     return True
 
-print is_prime(7)
+def prime_creator(num_list):
 
+    # i = 0
+    # for num in num_list:
+    #     if is_prime(num):
+    #         # prime_list.append(num)
+    #         i += 1
+    #         if i == 10000:
+    #             print num
+    #             break
 
-def prime_collector(num_range):
-    prime_list = []
-
-    for num in num_range:
+    num = 1
+    prime_count = 0
+    while prime_count < 10000:
+        num += 1
         if is_prime(num):
-            prime_list.append(num)
+            prime_count += 1
 
-    print prime_list[10001]
+    print num
 
-prime_collector(range(2,10000))
+
+
+
+num_list_ex = range(2,100)
+
+prime_creator(num_list_ex)
+
+
+#bad stuff ahead
+# def prime_collector(num_range):
+#     prime_list = []
+#
+#     for num in num_range:
+#         if is_prime(num):
+#             prime_list.append(num)
+#
+#     print prime_list[10001]
+#
+# prime_collector(range(2,10000))
