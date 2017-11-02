e1aa1534d97bd4da41bf42e7df8d309b59e61bda
Joe Plonsker <joeplonsker@Joes-MacBook-Pro.local>
2017-11-02 14:13:37 -0400

    create alternative solution for 9

--- python_solutions/9.py
+++ python_solutions/9.py
@@ -34,38 +34,23 @@
 #
 # trip_finder()
 
-
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+#
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+#
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
 def trip_finder():
-    # the basic logic
-    num_list = range(200,1001)
-
-    for num in num_list:
-        for num_2 in num_list:
-            for num_3 in num_list:
-                if num < num_2 < num_3:
-                    if num**2 + (num_2)**2 == (num_3)**2 and num + num_2 + num_3 == 1000:
-                        print(num)
-                        print(num_2)
-                        print(num_3)
+    for a in range(325,675):
+        for b in range(325,675):
+            for c in range(325,675):
+                if a<b and b<c:
+                    if a**2 + b**2 == c**2 and a + b + c == 1000:
+                        print(a)
+                        print(b)
+                        print(c)
                         break
-                    else:
-                        print("")
-                        print(num)
-                        print(num_2)
-                        print(num_3)
-                        print("")
-                        print("-----------")
-                        print("~----------")
-                        print("-~---------")
-                        print("--~--------")
-                        print("---~-------")
-                        print("----~------")
-                        print("-----~-----")
-                        print("------~----")
-                        print("-------~---")
-                        print("--------~--")
-                        print("---------~-")
-                        print("----------~")
 
 
 
